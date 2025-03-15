a7ec4ea0e0472492b9f326e58738e258b69452df
jlimb001 <jugallimbu18@gmail.com>
2025-03-15 19:26:09 +0000

    Solved lab

--- sub_queries.sql
+++ sub_queries.sql
@@ -54,6 +54,36 @@ JOIN film_category fc ON fc.film_id = f.film_id
 JOIN category c ON c.category_id = fc.category_id
 WHERE c.name = 'Family';
 
+-- Bonus: Retrieve the name and email of customers from Canada using both subqueries and joins.
+-- To use joins, you will need to identify the relevant tables and their primary and foreign keys.
 
+-- Using subqueries
+SELECT 
+    first_name, last_name, email
+FROM
+    customer
+WHERE
+    address_id IN (SELECT 
+            address_id
+        FROM
+            address
+        WHERE
+            city_id IN (SELECT 
+                    city_id
+                FROM
+                    city
+                WHERE
+                    country_id = (SELECT 
+                            country_id
+                        FROM
+                            country
+                        WHERE
+                            country = 'Canada')));
 
-
+-- Using joins
+SELECT c.first_name, c.last_name, c.email
+FROM customer c
+JOIN address a ON c.address_id = a.address_id
+JOIN city ci ON a.city_id = ci.city_id
+JOIN country co ON ci.country_id = co.country_id
+WHERE co.country = 'Canada';
